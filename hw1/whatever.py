62bf76c257f232bf3ebc7ebaa55bc7f293f86583
Dan Frakes <dpfrakes@gmail.com>
2018-01-28 10:37:31 -0500

    Done!

--- hw1/whatever.py
+++ hw1/whatever.py
@@ -13,21 +13,17 @@ def run_program(N, B):
     threshold = 0.01
 
     # Iterator variables
-    timestep = 0
-    max_timesteps = 0
+    max_timestep = 0
 
     # Calculate maximum likely horizon using arbitrary threshold
     while True:
-        p = pow(1. * (N - sum(B)) / N, timestep)
+        p = pow(1. * (N - sum(B)) / N, max_timestep)
         if p < threshold:
-            max_timesteps = timestep - 1
             break
-        timestep += 1
-
-    # print 'Threshold =', threshold, ': Max Timesteps =', max_timesteps
+        max_timestep += 1
 
     # State is bankroll, from 0 to N*max_timesteps (inclusive)
-    states = range(N * max_timesteps + 1)
+    states = range(N * max_timestep + 1)
 
     # Extra state for each possible bankroll to indicate terminal state
     states *= 2
@@ -45,32 +41,17 @@ def run_program(N, B):
     epsilon = 0.01
 
     vi = mdp.ValueIteration(T, R, gamma, epsilon, max_iter=1000)
-    vi.setVerbose()
     vi.run()
 
-    # last_roll = 0
-    # i = 0
-    # for r in vi.policy:
-    #     if r == 1:
-    #         last_roll = i
-    #         break
-    #     i += 1
-    # print 'Optimal policy: roll until bankroll = {}'.format(last_roll)
-    # print 'Expected value of first state:', vi.V[0]
-
-    print 'N={} ... output={}'.format(N, vi.R[0][0])
-
-    # print 'State values:'
-    # for i in range(len(vi.V)):
-    #     print 'state {} : {}'.format(i, vi.V[i])
+    print 'N={} ... output={}'.format(N, vi.V[0])
 
 
 ''' Let's do this thing'''
-e = dummy[0]
-run_program(e['N'], e['B'])
+# e = examples[0]
+# run_program(e['N'], e['B'])
 
-# for e in examples:
-#     run_program(e['N'], e['B'])
+for e in examples:
+    run_program(e['N'], e['B'])
 
 # for p in problems:
 #     run_program(p['N'], p['B'])
