73f59d0481b834fcddb8c2888291a9a5c1a6656b
dpfrakes <dpfrakes@gmail.com>
2018-02-24 15:57:46 -0500

    Use construction method to reconstruct Littman's figure 2.3

--- hw3/main.py
+++ hw3/main.py
@@ -1,53 +1,49 @@
-from problems import *
-from util import mdp_to_json
+from hw3.util import *
 
 
-def generate_problem():
-    mdp = {}
+SAVE_JSON = True
 
-    # T, R = get_sample_problem()
-    T, R = get_littman_problem()
+mdp = {}
 
-    assert(T.shape[1] == T.shape[2]), 'Transition matrix does not have square action ndarray'
-    assert(R.shape[1] == R.shape[2]), 'Reward matrix does not have square action ndarray'
+T, R = construct_TR(30)
 
-    na = T.shape[0]
-    ns = T.shape[1]
+assert T.shape[1] == T.shape[2], 'Transition matrix does not have square action ndarray'
+assert R.shape[1] == R.shape[2], 'Reward matrix does not have square action ndarray'
 
-    states = []
+na = T.shape[0]
+ns = T.shape[1]
 
-    for s in range(len(T[0][0])):
+states = []
 
-        state = {'id': s}
-        actions = []
+for s in range(len(T[0][0])):
 
-        for a in range(len(T)):
+    state = {'id': s}
+    actions = []
 
-            action = {'id': a}
-            transitions = []
-            transition_idx = 0  # to_idx not guaranteed if prob == 0
+    for a in range(len(T)):
 
-            for to_idx in range(ns):
-                probability = round(T[a, s, to_idx], 2)
-                if probability > 0:
-                    transitions.append({
-                        'id': transition_idx,
-                        'probability': probability,
-                        'reward': R[a, s, to_idx],
-                        'to': to_idx
-                    })
-                    transition_idx += 1
+        action = {'id': a}
+        transitions = []
+        transition_idx = 0  # to_idx not guaranteed if prob == 0
 
-            action['transitions'] = transitions
-            actions.append(action)
+        for to_idx in range(ns):
+            probability = round(T[a, s, to_idx], 2)
+            if probability > 0:
+                transitions.append({
+                    'id': transition_idx,
+                    'probability': probability,
+                    'reward': R[a, s, to_idx],
+                    'to': to_idx
+                })
+                transition_idx += 1
 
-        state['actions'] = actions
-        states.append(state)
+        action['transitions'] = transitions
+        actions.append(action)
 
-    mdp['gamma'] = 0.75
-    mdp['states'] = states
+    state['actions'] = actions
+    states.append(state)
 
-    return mdp_to_json(mdp, True)
+mdp['gamma'] = 0.75
+mdp['states'] = states
 
-
-problem = generate_problem()
+mdp_to_json(mdp, SAVE_JSON)
--- hw3/util.py
+++ hw3/util.py
@@ -1,5 +1,53 @@
 import json
 
+import numpy as np
+
+
+def construct_TR(ns):
+    """
+    Reconstruct Figure 2.3 from Littman's thesis (page 42)
+    with any (even) number of states > 3
+    :param ns: int - number of states
+    :return:
+    """
+
+    ''' Transition function '''
+    T1 = np.zeros((ns, ns))
+    T2 = np.zeros((ns, ns))
+
+    # Initial state
+    T1[0, 1] = 1.0
+    T2[0, 2] = 1.0
+
+    # Decision states
+    for s in range(1, ns - 3):
+        if s % 2 == 0:
+            T1[s, s + 1] = 0.5
+            T1[s, s + 2] = 0.5
+            T2[s, s + 1] = 0.5
+            T2[s, s + 2] = 0.5
+        else:
+            T1[s, s + 2] = 1.0
+            T2[s, s + 3] = 1.0
+
+    # Absorbing states
+    for s in range(ns - 3, ns):
+        T1[s, ns - 1] = 1.0
+        T2[s, ns - 1] = 1.0
+
+    T = np.array([T1, T2])
+
+    ''' Reward function '''
+    R1 = np.zeros((ns, ns))
+    R2 = np.zeros((ns, ns))
+
+    R1[ns - 3, ns - 1] = -1
+    R2[ns - 3, ns - 1] = -1
+
+    R = np.array([R1, R2])
+
+    return T, R
+
 
 def mdp_to_json(mdp_obj, save=False):
     if save:
