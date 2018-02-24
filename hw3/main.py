d06972501e8176c6f7c0e1485b4310497b989a30
dpfrakes <dpfrakes@gmail.com>
2018-02-24 16:01:47 -0500

    Refactor

--- hw3/main.py
+++ hw3/main.py
@@ -3,47 +3,10 @@ from hw3.util import *
 
 SAVE_JSON = True
 
-mdp = {}
-
 T, R = construct_TR(30)
 
 assert T.shape[1] == T.shape[2], 'Transition matrix does not have square action ndarray'
 assert R.shape[1] == R.shape[2], 'Reward matrix does not have square action ndarray'
 
-na = T.shape[0]
-ns = T.shape[1]
-
-states = []
-
-for s in range(len(T[0][0])):
-
-    state = {'id': s}
-    actions = []
-
-    for a in range(len(T)):
-
-        action = {'id': a}
-        transitions = []
-        transition_idx = 0  # to_idx not guaranteed if prob == 0
-
-        for to_idx in range(ns):
-            probability = round(T[a, s, to_idx], 2)
-            if probability > 0:
-                transitions.append({
-                    'id': transition_idx,
-                    'probability': probability,
-                    'reward': R[a, s, to_idx],
-                    'to': to_idx
-                })
-                transition_idx += 1
-
-        action['transitions'] = transitions
-        actions.append(action)
-
-    state['actions'] = actions
-    states.append(state)
-
-mdp['gamma'] = 0.75
-mdp['states'] = states
-
-mdp_to_json(mdp, SAVE_JSON)
+mdp_obj = construct_mdp_obj(T, R)
+mdp_to_json(mdp_obj, SAVE_JSON)
--- hw3/util.py
+++ hw3/util.py
@@ -49,6 +49,46 @@ def construct_TR(ns):
     return T, R
 
 
+def construct_mdp_obj(T, R):
+
+    mdp = {}
+    states = []
+
+    ns = T.shape[1]
+
+    for s in range(len(T[0][0])):
+
+        state = {'id': s}
+        actions = []
+
+        for a in range(len(T)):
+
+            action = {'id': a}
+            transitions = []
+            transition_idx = 0  # to_idx not guaranteed if prob == 0
+
+            for to_idx in range(ns):
+                probability = round(T[a, s, to_idx], 2)
+                if probability > 0:
+                    transitions.append({
+                        'id': transition_idx,
+                        'probability': probability,
+                        'reward': R[a, s, to_idx],
+                        'to': to_idx
+                    })
+                    transition_idx += 1
+
+            action['transitions'] = transitions
+            actions.append(action)
+
+        state['actions'] = actions
+        states.append(state)
+    mdp['gamma'] = 0.75
+    mdp['states'] = states
+
+    return mdp
+
+
 def mdp_to_json(mdp_obj, save=False):
     if save:
         with open('problem.json', 'w') as outfile:
