0f40d618f679c38d0a9aa773b05acea57d92af71
dpfrakes <dpfrakes@gmail.com>
2018-03-24 00:00:55 -0400

    Additional problems, fixed agent by removing assumption about no-fight label

--- hw5/main.py
+++ hw5/main.py
@@ -11,6 +11,7 @@ class KWIKFightDetector:
     def __init__(self, max_unknown):
         # Agent memory fields based on data from first prediction
         self.initialized = False
+        self.memory = None
 
         # Label request upper bound
         self.max_unknown = max_unknown
@@ -23,6 +24,9 @@ class KWIKFightDetector:
 
     def _setup(self, num_patrons):
         self.initialized = True
+
+        # Remember labeled training data
+        self.memory = np.empty((0, num_patrons + 1))
 
         # Track discovered instigator and peacemaker patron indices
         self.possible_instigators = list(range(num_patrons))
@@ -48,18 +52,18 @@ class KWIKFightDetector:
             return NO_FIGHT
 
         # Convert to list of indices ("patron IDs")
-        patrons = np.asarray(np.where(patrons)).flatten()
+        patron_ids = np.asarray(np.where(patrons)).flatten()
 
         if self.known_peacemaker is not False:
 
             # Peacemaker is present
-            if self.known_peacemaker in patrons:
+            if self.known_peacemaker in patron_ids:
                 return NO_FIGHT
 
             if self.known_instigator is not False:
 
                 # Instigator present and peacemaker is not
-                if self.known_instigator in patrons:
+                if self.known_instigator in patron_ids:
                     return FIGHT
 
                 # Instigator not present
@@ -68,13 +72,21 @@ class KWIKFightDetector:
         if self.known_instigator is not False:
 
             # Instigator is not present
-            if self.known_instigator not in patrons:
+            if self.known_instigator not in patron_ids:
                 return NO_FIGHT
+
+        # Check memory if we've seen this before
+        for m in self.memory:
+            if np.all(m[:-1] == patrons):
+                return int(m[-1])
 
         # No immediate information and insufficient historical data
         return UNKNOWN
 
     def train(self, patrons, fight):
+        learned_instigator = False
+        learned_peacemaker = False
+
         if fight:
             # If fight occurred, we know instigator was present and
             # peacemaker is absent
@@ -82,14 +94,23 @@ class KWIKFightDetector:
             self.possible_peacemakers = np.intersect1d(self.possible_peacemakers, np.where(np.invert(patrons)))
 
         else:
-            # If no fight occurred, we know instigator was not present
-            self.possible_instigators = np.intersect1d(self.possible_instigators, np.where(np.invert(patrons)))
+            # If no fight occurred, we know (instigator was not present) OR (instigator AND peacemaker are present)
+            if self.known_instigator is not False and self.known_instigator in np.where(patrons):
+                self.possible_peacemakers = np.intersect1d(self.possible_peacemakers, np.where(patrons))
+
+        # Save to memory
+        record = np.append(patrons, fight)
+        self.memory = np.vstack([self.memory, record])
 
         # Narrow down suspects to 1
         if len(self.possible_instigators) == 1:
             self.known_instigator = self.possible_instigators[0]
+            learned_instigator = True
         if len(self.possible_peacemakers) == 1:
             self.known_peacemaker = self.possible_peacemakers[0]
+            learned_peacemaker = True
+
+        return learned_instigator, learned_peacemaker
 
 
 if __name__ == '__main__':
@@ -103,6 +124,13 @@ if __name__ == '__main__':
 
         agent = KWIKFightDetector(len(all_patrons[0]))
 
+        print('*' * 80)
+        print('Problem {}'.format(p + 1))
+        print('{} episodes'.format(num_episodes))
+        print('{} patrons'.format(len(all_patrons[0])))
+
+        assert len(all_patrons) == len(all_fights)
+
         for e in range(num_episodes):
             patron_list = np.array(all_patrons[e], dtype=bool)
             fight_occurred = np.array(all_fights[e], dtype=bool)
@@ -111,6 +139,13 @@ if __name__ == '__main__':
             problem_predictions.append(prediction)
             if prediction == UNKNOWN:
                 # print('Agent cannot predict: training agent with labeled episode data...')
-                agent.train(patron_list, fight_occurred)
+                instigator, peacemaker = agent.train(patron_list, fight_occurred)
 
-        print('Problem {}: {}'.format(p, problem_predictions))
+                # Check training information gain
+                if instigator:
+                    print('Identified instigator after {} episodes'.format(e + 1))
+                if peacemaker:
+                    print('Identified peacemaker after {} episodes'.format(e + 1))
+
+        print('Solution:\n{}'.format(problem_predictions))
+        print()
