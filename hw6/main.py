08b66f3b67a3b3d98584451b9cb6d016af59aa8a
dpfrakes <dpfrakes@gmail.com>
2018-04-07 19:09:53 -0400

    Suppress output, refactor to look like respectable code

--- hw6/main.py
+++ hw6/main.py
@@ -1,4 +1,4 @@
-from cvxopt import matrix
+from cvxopt import matrix, solvers
 from cvxopt.modeling import dot, op, variable
 import numpy as np
 
@@ -14,42 +14,55 @@ S  [ 1, -1,  0]]
 
 
 """
+def solve(problem):
+    # Variables
+    R = variable(1, 'R')
+    P = variable(1, 'P')
+    S = variable(1, 'S')
+    V = variable(1, 'V')
 
-# Variables
-R = variable(1, 'R')
-P = variable(1, 'P')
-S = variable(1, 'S')
-V = variable(1, 'V')
+    # Variable vector [R, P, S, V]
+    x = variable(4)
 
-# Variable vector [R, P, S, V]
-x = variable(4)
+    # minimize cTx == -V
+    c = matrix([0., 0., 0., -1.])
+    objective = dot(c, x)
 
-# minimize cTx == -V
-c = matrix([0., 0., 0., -1.])
-objective = dot(c, x)
+    # Matrices
+    Ainit = np.zeros((0, len(x)))
 
-# Matrices
-Ainit = np.zeros((0, len(x)))
+    # R = 0, P = 1, S = 2, V = V
+    for row in problem:
+        Ainit = np.vstack([Ainit, [row[0], row[1], row[2], 1]])
 
-# R = 0, P = 1, S = 2, V = V
-for row in sample_problems[0]:
-    Ainit = np.vstack([Ainit, [row[0], row[1], row[2], 1]])
+    # Other known constraints
+    Ainit = np.vstack([Ainit, [1., 1., 1., 0.]])  # R + P + S <= 1
+    # Ainit = np.vstack([Ainit, [-1., 0., 0., 0.]])  # -R <= 0
+    # Ainit = np.vstack([Ainit, [0., -1., 0., 0.]])  # -P <= 0
+    # Ainit = np.vstack([Ainit, [0., 0., -1., 0.]])  # -S <= 0
 
-# Other known constraints
-Ainit = np.vstack([Ainit, [1., 1., 1., 0.]])  # R + P + S <= 1
-# Ainit = np.vstack([Ainit, [-1., 0., 0., 0.]])  # -R <= 0
-# Ainit = np.vstack([Ainit, [0., -1., 0., 0.]])  # -P <= 0
-# Ainit = np.vstack([Ainit, [0., 0., -1., 0.]])  # -S <= 0
+    A = matrix(Ainit)
+    # b = matrix([0., 0., 0., 1., 0., 0., 0.])
+    b = matrix([0., 0., 0., 1.])
 
-A = matrix(Ainit)
-# b = matrix([0., 0., 0., 1., 0., 0., 0.])
-b = matrix([0., 0., 0., 1.])
+    # Linear constraints embedded in matrices
+    ineq = (A*x <= b)
 
-# Linear constraints embedded in matrices
-ineq = (A*x <= b)
+    lp = op(objective, ineq)
+    lp.solve()
 
-problem = op(objective, ineq)
-problem.solve()
+    # [R, P, S, V] ^ T
+    return ineq.multiplier.value
 
-# [R, P, S, V] ^ T
-print(ineq.multiplier.value)
+
+if __name__ == '__main__':
+    solvers.options['show_progress'] = False
+
+    for i, problem in enumerate(sample_problems):
+        print('Problem {}'.format(i + 1))
+        solution = solve(problem)
+        print([round(x, 5) for x in solution])
+        # print('R={} P={} S={} V={}'.format(solution[0], solution[1], solution[2], solution[3]))
+
+        if i + 1 < len(sample_problems):
+            print()
